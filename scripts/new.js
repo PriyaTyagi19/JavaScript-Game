ed5a61b483355d9af61bc5cc015dbab1b9fd303b
Priya Tyagi <priyatyagi1905@gmail.com>
2024-03-18 12:28:05 -0700

    completed the styling and make levels for the game

--- scripts/new.js
+++ scripts/new.js
@@ -10,90 +10,58 @@ const btn5 = document.getElementById("button5");
 const btn6 = document.getElementById("button6");
 const moveCounter = document.querySelector(".moves");
 const cardContainer = document.getElementById("cards");
-const cards8 = document.querySelectorAll(".cards8");
-const cards12 = document.querySelectorAll(".cards12");
-const cards16 = document.querySelectorAll(".cards16");
 
-const emojis16 = ["😊", "😂", "😍", "👌", "🙌", "😎", "😜", "❤️"];
-const emojis12 = ["😊", "😂", "😍", "👌", "🙌", "😎"];
-const emojis8 = ["😊", "😂", "😍", "👌"];
-
-let emojiCount; 
-let cardArray = [];
-// console.log(emojisGroup);
-
+const emojis = ["😊", "😂", "😍", "👌", "🙌", "😎", "😜", "❤️"];
+const emojisGroup = [...emojis, ...emojis];
+const emojiCount = emojisGroup.length;
 
 // for enter user name
 const addPlayer = function () {
-  const nameShow = document.getElementById("name");
-  const playerName = nameShow.value;
-  nameShow.value = "";
-
-  if (playerName.length <= 0) {
-    alert("Plaese enter a name");
-    return;
-  }
-  levelSelection(playerName);
-  levelSelected.querySelector("span").textContent = playerName;
-};
-
-const levelSelection = function () {
-  player.style.display = "none";
-  levelSelected.style.display = "block";
-};
-
-button.addEventListener("click", addPlayer);
-
-
-// to change levels
-const switchLevels = function () {
-  levelSelected.style.display = "block";
-  gameSection.style.display = "none";
-
-};
-
-btn5.addEventListener("click", switchLevels);
-
-
-
-// to show game screen
-
-
-const gameScreen = function (numberOfCards) {
-  levelSelected.style.display = "none";
-  gameSection.style.display = "block";
-    cardContainer.innerHTML = '';
-    
-    if (numberOfCards == 16){
-      cardArray = [...emojis16, ...emojis16];
-    } else if (numberOfCards == 12){
-      cardArray = [...emojis12, ...emojis12];
-    } else {
-      cardArray = [...emojis8, ...emojis8];
-    }
-    shuffle(cardArray);
-    emojiCount= cardArray.length;
-    moves= 0; //reset moves counter
-    moveCounter.textContent = `Number of moves: ${moves}`; // update move counter display
-};
-
-btn1.addEventListener("click", function (){
+    const nameShow = document.getElementById("name");
+    const playerName = nameShow.value;
+    nameShow.value = "";
   
-  gameScreen(8);
-});
+    if (playerName.length <= 0) {
+      alert("Plaese enter a name");
+      return;
+    }
+    levelSelection(playerName);
+    levelSelected.querySelector("span").textContent = playerName;
+  };
+  
+  const levelSelection = function () {
+    player.style.display = "none";
+    levelSelected.style.display = "block";
+  };
+  
+  button.addEventListener("click", addPlayer);
+  
+  
+  // to change levels
+  const switchLevels = function () {
+    levelSelected.style.display = "block";
+    gameSection.style.display = "none";
+  
+  };
+  
+  btn5.addEventListener("click", switchLevels);
 
-btn2.addEventListener("click", function (){
-  gameScreen(12);
-});
+  const gameScreen = function () {
+    levelSelected.style.display = "none";
+    gameSection.style.display = "block";
+      cardContainer.innerHTML = '';
+      shuffle(emojisGroup);
+      moves= 0; //reset moves counter
+      moveCounter.textContent = `Number of moves: ${moves}`; // update move counter display
+  };
+  
+  btn1.addEventListener("click", gameScreen);
+  
+  btn2.addEventListener("click", gameScreen);
+  
+  btn3.addEventListener("click", gameScreen);
 
-btn3.addEventListener("click", function (){
-  gameScreen(16);
-});
-
-
-
-
-// game state
+  // game state
 let revealCount = 0;
 let activeTile = null;
 let awaitingendOfMove = false;
@@ -101,7 +69,7 @@ let moves = 0;
 
 // to shuffle the emojis
 function shuffle(array) {
-  let currentIndex = array.length, randomIndex;
+  let currentIndex = emojiCount, randomIndex;
   while (currentIndex > 0) {
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
@@ -114,7 +82,7 @@ function shuffle(array) {
     // add card div with random emojis value in main cards div
 
    
-    let emoji = array[currentIndex];
+    let emoji = emojisGroup[currentIndex];
     let tile = addCards(emoji);
     cardContainer.appendChild(tile);
     tile.style.backgroundImage = "url('images/smiley1.jpg')";
@@ -125,7 +93,7 @@ function shuffle(array) {
 
 }
 
-shuffle(cardArray);
+shuffle(emojisGroup);
 // console.log(emojisGroup);
 
 
@@ -133,20 +101,10 @@ shuffle(cardArray);
 // add emojis as cards in the game window
 function addCards(emoji) {
   const element = document.createElement("div");
-  
-  if (cardArray == emojis16) {
-
-    element.classList.add("cards16");
-
-  } else if (cardArray == emojis12) {
-
-    element.classList.add("cards12");
-  } else {
-    element.classList.add("cards8");
-
-  }
+  element.classList.add("card");
   element.setAttribute("data-value", emoji);
   element.setAttribute("data-revealed", "false");
+  
   
   // to turn the tiles on click
   element.addEventListener('click', function(){
@@ -213,7 +171,7 @@ function addCards(emoji) {
 function restartGame (){
  
   cardContainer.innerHTML = '';
-  shuffle(cardArray);
+  shuffle(emojisGroup);
   moves= 0; //reset moves counter
   moveCounter.textContent = `Number of moves: ${moves}`; // update move counter display
 }
@@ -231,3 +189,5 @@ btn6.addEventListener("click", function () {
     gameSection.style.display = "none";
   }
 });
+
+  
